21decd6146243a995a61be7008ef8a79458b6aeb
valnuccio <val.nuccio@gmail.com>
2020-08-05 21:54:38 -0400

    done with assistance. ask one question. annotated

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+    attr_accessor :name, :roster
+  
+    def initialize(name)
+      @name = name
+      @roster = {}
+    end
+  # not sure about this next bit - remember to ask in class why this fixed it. 
+    def add_student(student_name, grade)
+        roster[grade] ||= []
+        roster[grade] << student_name
+      end
+
+
+    def grade(student_grade)
+      roster[student_grade]
+    end
+  
+    def sort
+      hash = {}
+      roster.each do |grade, students|
+        hash[grade] = students.sort
+      end
+      hash
+    end
+  end
+  
